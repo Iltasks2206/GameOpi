d79ed79f7a5f6d1c3647f9b7bd4ceec6ccc6b08b
Iltasks <alex.sazonov.13@yandex.ru>
2022-12-09 23:28:34 +0300

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <stdexcept>
 #include <string>
+#include <math.h>
 #include <Windows.h>
+#include <map>
 #include <time.h>
+#include <limits.h>
 #include <fstream>
 
 using namespace std;
 
 const string str = "_________________________";
-const wstring drum[16] = {L"100", L"120",  L"140", L"150", L"160", L"180", L"200", L"220", L"240", L"250", L"260", L"280", L"300", L"+", L"переход хода"};
+const wstring drum[15] = { L"100", L"120",  L"140", L"150", L"160", L"180", L"200", L"220", L"240", L"250", L"260", L"280", L"300", L"+", L"переход хода" };
 const wstring OutputDrum = L"НА БАРАБАНЕ";
- 
+static map <wchar_t, bool> Letters;
+
 struct question {
 
     string que, ans;
@@ -26,30 +31,30 @@ struct question {
 vector <question> InputQuestionsFromTheTextFile()
 {
     ifstream file("Questions.txt");
-    
+
     if (!file.is_open()) {
         wcout << L"Ошибка открытия файла с вопросами!\n";
         exit(false);
-    } 
+    }
 
     vector <question> Questions;
-    while(!file.eof())
+    while (!file.eof())
     {
         string line;
         getline(file, line);
 
         int PosOfDoublet = line.find(':');
         string que = line.substr(0, PosOfDoublet + 1);
-        while(que[0] == ' ') que.erase(que.begin());
-        while(que[que.size() - 1] == ' ') que.erase(que.end() - 1);
+        while (que[0] == ' ') que.erase(que.begin());
+        while (que[que.size() - 1] == ' ') que.erase(que.end() - 1);
 
         string ans = line.substr(PosOfDoublet + 2, line.size() - PosOfDoublet - 1);
-        while(ans[0] == ' ') ans.erase(ans.begin());
-        while(ans[ans.size() - 1] == ' ') ans.erase(ans.end() - 1);
+        while (ans[0] == ' ') ans.erase(ans.begin());
+        while (ans[ans.size() - 1] == ' ') ans.erase(ans.end() - 1);
 
         Questions.push_back(question(que, ans));
     }
-    
+
     file.close();
 
     return Questions;
@@ -58,32 +63,123 @@ vector <question> InputQuestionsFromTheTextFile()
 void pause()
 {
     cin.clear();
-    while(cin.get() != '\n');
+    while (cin.get() != '\n');
+}
+
+void Print(string que, string s, const vector <int> PlayersScore)
+{
+    cout << que << "    ";
+    for (auto i : s) cout << i << ' ';
+    cout << "\n";
+    wcout << L"Баланс игроков:        1 игрок         2 игрок         3 игрок\n";
+    cout << "                          " << PlayersScore[0] << "               " << PlayersScore[1] << "               " << PlayersScore[2] << "\n";
 }
 
 wstring SpinDrum()
 {
-    wstring Points = drum[rand() % 16];
+    wstring Points = drum[rand() % 15];
     return Points;
 }
 
-void PlusOnDrum(vector <bool>& Letters, string& s, string& ans) {
-    int i = 0;
-}
-
-void PointsOnDrum(vector <bool>& Letters, string& s, string& ans, vector <int>& Players) {
-
-}
-
-void Print(string que, string s, const vector <int> PlayersScore) 
+void PlusOnDrum(string& que, string& s, string& ans, int& CurPlayer) 
 {
-    setlocale(LC_ALL, "Russian");
-
     cout << que << "    ";
-    for(auto i: s) cout << i << ' ';
+    for (auto i : s) cout << i << ' ';
     cout << "\n";
-    wcout << L"Баланс игроков:        1 игрок         2 игрок         3 игрок\n";
-    cout << "                          " << PlayersScore[0] << "               " << PlayersScore[1] << "               " << PlayersScore[2] << "\n"; 
+
+    wcout << L"Выбирите номер буквы, которую хотите открыть            ";
+    int a;
+    do {
+        try
+        {
+            string test;
+            getline(cin, test);
+            int myint = stoi(test);
+            cout << myint << ' ' << test << "\n";
+            if (myint > 0 && myint <= ans.size()) {
+                a = myint;
+                break;
+            }
+            else {
+                wcout << L"Вы ввели что-то не понятное, введите номер буквы еще раз            ";
+                continue;
+            }
+        }
+        catch (std::exception& e)
+        {
+            wcout << L"Вы ввели что-то не понятное, введите номер буквы еще раз            ";
+            continue;
+        }
+
+    } while (true);
+
+    if (s[a - 1] != '_') {
+        wcout << L"Вы конечно умный! Такая буква уже открыта.\n";
+        CurPlayer = (CurPlayer + 1) % 3;
+    }
+    else {
+        wcout << L"Открыть букву под номером " << a << "\n";
+
+        for (int i = 0; i < ans.size(); i++) {
+            if (ans[i] == ans[a - 1]) {
+                s[i] = ans[i];
+            }
+        }
+        cout << que << "    ";
+        for (auto i : s) cout << i << ' ';
+        cout << "\n";
+    }
+
+}
+
+void PointsOnDrum(string& que, string& s,
+    string& ans, vector <int>& PlayersScore,
+    int& CurPlayer, wstring Points) {
+
+    char io = ' ';
+    string u = "";
+    bool flag = false;
+    do {
+        if (flag) wcout << L"Я не расслышал. Повторите еще раз.\n";
+        wcout << L"БУКВА(Заглавная буква от А до Я)          ";
+        cin.clear();
+        getline(cin, u);
+        if (u.size() >= 2) {
+            while (u[0] == ' ') u.erase(u.begin());
+            while (u.size() > 1 && u[u.size() - 1] == ' ') u.erase(u.end() - 1);
+        }
+        flag = true;
+        if (u.size() > 1) continue;
+        io = u[0];
+    } while ('А' - 64 > io ||  io > 'Я' - 64);
+
+    if (Letters[io]) {
+        wcout << L"Вы конечно умный! Такая буква уже была.\n";
+        CurPlayer = (CurPlayer + 1) % 3;
+    }
+    else {
+        Letters[io] = true;
+        int k = 0;
+        bool f = false;
+        for (int i = 0; i < ans.size(); i++) {
+            if (ans[i] - 64 == io) {
+                k++;
+                f = true;
+                s[i] = io + 64;
+            }
+        }
+
+        if (f) {
+            wcout << L"Есть такая буква.\n";
+            PlayersScore[CurPlayer] += k * stoi(Points);
+            Print(que, s, PlayersScore);
+        }
+        else {
+            wcout << L"Нет такой буквы.\n";
+            CurPlayer = (CurPlayer + 1) % 3;
+        }
+    }
+
 }
 
 question ChoosingRandomQuestion(vector <question>& Questions)
@@ -91,7 +187,7 @@ question ChoosingRandomQuestion(vector <question>& Questions)
     int NumOfQue = rand() % Questions.size();
     string que = Questions[NumOfQue].que;
     string ans = Questions[NumOfQue].ans;
-    Questions.erase(Questions.begin() + NumOfQue); 
+    Questions.erase(Questions.begin() + NumOfQue);
 
     return question(que, ans);
 }
@@ -108,26 +204,37 @@ int main()
     vector <int> PlayersScore(3, 0);
     int CurPlayer = 0;
     Print(que, s, PlayersScore);
-    vector <bool> Letters(26, false);
 
-    while(true) {
+    while (true) {
+
+        bool f = true;
+        for (char i : s) {
+            if (i == '_') {
+                f = false;
+                break;
+            }
+        }
+        if (f) break;
 
         wstring Points = SpinDrum();
         wcout << L"Ход " << CurPlayer + 1 << L"-го игрока           " << OutputDrum << "               " << Points;
-        
+        pause();
+
         if (Points == L"переход хода") {
             CurPlayer = (CurPlayer + 1) % 3;
-            pause();
             continue;
-        } else if (Points == L"+") {
-            
-        } else {
+        }
+        else if (Points == L"+") {
+            PlusOnDrum(que, s, ans, CurPlayer);
+        }
+        else {
+            PointsOnDrum(que, s, ans, PlayersScore, CurPlayer, Points);
             continue;
         }
 
-        pause();
-
     }
-    
+
+    wcout << L"Выиграл " << CurPlayer + 1 << L"-й игрок. Баланс игрока " << PlayersScore[CurPlayer] << L" очков. Ура-ура-ура ...\n";
+
     return 0;
 }
